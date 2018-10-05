84c0a103946275c5fbe14c8ce8535cd0787d9017
reda-hdp <37316717+reda-hdp@users.noreply.github.com>
2018-10-05 19:22:52 -0400

    mon commit

--- src/org/reda/Main.java
+++ src/org/reda/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-	    system.out.println("test github");
+	    system.out.println("test github 1");
     }
 }
